8db022c37b9119f6ef10e43e5a2e49f7a9268d34
Tom Bland <tom_bland@hotmail.co.uk>
2018-08-06 15:29:58 +0100

    changes to scripts

--- Run.sh
+++ Run.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --time=0
 #SBATCH --cpus-per-task=32
-#SBATCH --array=0-29
+#SBATCH --array=0-149
 
 module load Python/3.5.2-foss-2016b
 
-srun python -m Scripts.s180725__c__m0008_random $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_MAX
+srun python -m Scripts.s180806__c__m0008b_random $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_MAX
