d6163872d73e46cdb457c0faf4bea14aa7da6b60
Tom Bland <tom_bland@hotmail.co.uk>
2018-08-06 23:20:06 +0100

    change to script

--- Scripts/s180806__c__m0008b_random.py
+++ Scripts/s180806__c__m0008b_random.py
@@ -24,11 +24,11 @@ params = ['ra', 'kon_p', 'kon_p_2', 'kd_f', 'kd_b', 'rp']
 ranges = [[0.1, 10], [0.0001, 0.01], [0.1, 10], [0.1, 10], [0.1, 10], [0.1, 10]]
 
 
-# Standard simulation + analysis
-x.alg_parsim_rand_clust(m=m0008b.Model(p0), params=params, ranges=ranges, nsims=960, jobid=8, subjobid=0, cores=32,
-                        compression=0, node=int(sys.argv[1]))
-x.batch_analysis(8, 0, funcs=x.all_analysis)
-x.save_scores_batch(8, 0)
+# # Standard simulation + analysis
+# x.alg_parsim_rand_clust(m=m0008b.Model(p0), params=params, ranges=ranges, nsims=960, jobid=8, subjobid=0, cores=32,
+#                         compression=0, node=int(sys.argv[1]))
+# x.batch_analysis(8, 0, funcs=x.all_analysis)
+# x.save_scores_batch(8, 0)
 
 # Retesting in PAR-1 RNAi mode + analysis
 x.alg_parsim_clust_duplicate(m=m0008b.Model(p0), changes={'ra': 0}, oldjobid=8, oldsubjobid=0, newjobid=8,
