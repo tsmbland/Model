1905c6390817a2ff3c04bc3f833f9d782c4b0ea7
Tom Bland <tom_bland@hotmail.co.uk>
2018-08-06 22:33:36 +0100

    fix to m0008

--- Scripts/s180806__c__m0008b_random.py
+++ Scripts/s180806__c__m0008b_random.py
@@ -16,7 +16,7 @@ x.datadirec = '../working/Tom/ModelData'
 # Base parameter set
 p0 = m0008b.Params(Da=0.28, kon_a=0.0085, koff_a=0.0054, ra=1, Dp=0.15, kon_p=0.0474, koff_p=0.0073, kon_p_2=0.0474,
                    kd_f=1, kd_b=1, rp=1, L=67.3,
-                   xsteps=500, psi=0.174, Tmax=5000, deltat=0.01, starts=[0, 1.56, 0, 0, 0, 1, 0])
+                   xsteps=500, psi=0.174, Tmax=1000, deltat=0.01, starts=[0, 1.56, 0, 0, 0, 1, 0])
 
 # Free parameters
 params = ['ra', 'kon_p', 'kon_p_2', 'kd_f', 'kd_b', 'rp']
