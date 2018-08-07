4b4da2e38d7d4dfcd7c01ede9eaeeb9183892a75
Tom Bland <tom_bland@hotmail.co.uk>
2018-08-07 08:18:44 +0100

    change to script

--- Scripts/s180806__c__m0008b_random_no_diffusion.py
+++ Scripts/s180806__c__m0008b_random_no_diffusion.py
@@ -23,15 +23,15 @@ params = ['ra', 'kon_p', 'kon_p_2', 'kd_f', 'kd_b', 'rp']
 # ranges = [[0.1, 10], [0.0001, 0.01], [0.1, 10], [0.1, 10], [0.1, 10], [0.1, 10]]
 ranges = [[0.1, 10], [0.0001, 0.01], [0.1, 10], [0.1, 10], [0.1, 10], [0.1, 10]]
 
-# Standard simulation + analysis
-x.alg_parsim_rand_clust(m=m0008b.Model(p0), params=params, ranges=ranges, nsims=960, jobid=9, subjobid=0, cores=32,
-                        compression=0, node=int(sys.argv[1]))
-x.batch_analysis(9, 0, funcs=x.all_analysis)
-x.save_scores_batch(9, 0)
+# # Standard simulation + analysis
+# x.alg_parsim_rand_clust(m=m0008b.Model(p0), params=params, ranges=ranges, nsims=960, jobid=9, subjobid=0, cores=32,
+#                         compression=0, node=int(sys.argv[1]))
+# x.batch_analysis(9, 0, funcs=x.all_analysis)
+# x.save_scores_batch(9, 0)
 
-# # Retesting in PAR-1 RNAi mode + analysis
-# x.alg_parsim_clust_duplicate(m=m0008b.Model(p0), changes={'rp': 0}, oldjobid=9, oldsubjobid=0, newjobid=9,
-#                              newsubjobid=1,
-#                              cores=32, node=int(sys.argv[1]), compression=0)
-# x.batch_analysis(9, 1, funcs=x.all_analysis)
-# x.save_scores_batch(9, 1)
+# Retesting in PAR-1 RNAi mode + analysis
+x.alg_parsim_clust_duplicate(m=m0008b.Model(p0), changes={'rp': 0}, oldjobid=9, oldsubjobid=0, newjobid=9,
+                             newsubjobid=1,
+                             cores=32, node=int(sys.argv[1]), compression=0)
+x.batch_analysis(9, 1, funcs=x.all_analysis)
+x.save_scores_batch(9, 1)
