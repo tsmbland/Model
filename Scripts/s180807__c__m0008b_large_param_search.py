042e25dba69a557b0ad98ee4312766c5e4e84285
Tom Bland <tom_bland@hotmail.co.uk>
2018-08-07 10:35:19 +0100

    fix to m0008

--- Scripts/s180807__c__m0008b_large_param_search.py
+++ Scripts/s180807__c__m0008b_large_param_search.py
@@ -15,10 +15,11 @@ p0 = m0008b.Params(Da=0, kon_a=0.0085, koff_a=0.0054, ra=1, Dp=0, kon_p=0.0474, 
 
 # Free parameters
 params = ['ra', 'kon_p', 'kon_p_2', 'kd_f', 'kd_b', 'rp']
+# ranges = [[0.001, 10], [0.001, 10], [0.001, 10], [0.001, 10], [0.001, 10], [0.001, 10]]
 ranges = [[0.001, 0.1], [0.0001, 0.01], [0.1, 10], [0.01, 1], [0.01, 1], [0.0001, 0.01]]
 
 # Standard simulation + analysis
-x.alg_parsim_rand_clust(m=m0008b.Model(p0), params=params, ranges=ranges, nsims=2880, jobid=10, subjobid=0, cores=96,
+x.alg_parsim_rand_clust(m=m0008b.Model(p0), params=params, ranges=ranges, nsims=3200, jobid=10, subjobid=0, cores=32,
                         compression=0, node=int(sys.argv[1]))
 x.batch_analysis(10, 0, funcs=x.all_analysis)
 x.save_scores_batch(10, 0)
